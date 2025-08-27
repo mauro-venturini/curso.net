dd86d567cc516f3dfc8cde820642b13460b0009b
mauro venturini <joelventurini20@gmail.com>
2025-08-27 19:44:03 -0300

    commit prueba

---
+++ source/repos/PaginaVentas/Negocios/ControladorUsuario.cs
@@ -0,0 +1,20 @@
+﻿using Datos;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocios
+{
+    public class ControladorUsuario
+    {
+        DatosUsuario Dusuario = new DatosUsuario();
+        public int CrearUsuario(Usuario usuario)
+        {
+            return Dusuario.LoginUsuario(usuario);
+        }
+    }
+}
